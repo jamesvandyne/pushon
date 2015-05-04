0f0a855d0b1e535d5327d3252a971b4fa3a04edd
James Van Dyne <james@sugoisoft.com>
2015-05-04 11:48:55 -0500

    Adds a simple argument to build this to allow building for ios (android is default)

--- build_this.sh
+++ build_this.sh
@@ -2,5 +2,11 @@ npm install;
 grunt build;
 cordova plugins add https://github.com/urbanairship/phonegap-ua-push.git
 cordova restore plugins --experimental;
-cordova platform add android;
-cordova run android
+if [[ -n $1 ]] && [[ $1 == "ios" ]]
+then
+    cordova platform add ios;
+    cordova run ios
+else
+    cordova platform add android;
+    cordova run android
+fi
